e2cd91dc5bef9f773a18885758035b4710395493
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-01 11:18:56 +0900

    first

---
+++ src/main/java/jp/ac/hcs/s3a120/MusicApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
