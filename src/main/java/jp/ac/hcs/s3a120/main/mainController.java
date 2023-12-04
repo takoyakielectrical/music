ad2a43cd3ca32f17f997fc5aa4792f0639080e90
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-04 10:24:35 +0900

    途中経過

--- src/main/java/jp/ac/hcs/s3a120/main/mainController.java
+++ src/main/java/jp/ac/hcs/s3a120/main/mainController.java
@@ -2,11 +2,17 @@ package jp.ac.hcs.s3a120.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class mainController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@PostMapping("/play")
+	public String play() {
+		return "play";
 	}
 }
