401f60ab5dd83f767a37a5cd8912d636af9d5375
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-04 14:41:38 +0900

    いくぞ

--- src/main/resources/static/js/musicplay.js
+++ src/main/resources/static/js/musicplay.js
@@ -1,13 +1,36 @@
-use Strict
+
 /**
  * 
  */
- function play(){
-  const music = new Audio('./music/ああああ.wav');
- for (let step = 0; step < 5; step++) {
-  music.play();
-  /
-  }
+
+const urls = [];
+const musics = [];
+function play(url) {
+	urls.push(url);
+	for (const url of urls) {
+		const music = new Audio(url);
+		musics.push(music);
+	}
+	console.log(urls)
+
+
+	function playMusic() {
+		return new Promise((resolve) => {
+			for (const music of musics) {
+				music.addEventListener('ended', resolve); // 再生が終わったらresolve
+				music.play();
+				
+			}
+		});
+	}
+
+	async function playLoop() {
+		while (true) {
+			await playMusic();
+		}
+	}
+
+	playLoop();
 }
 
- 
+
