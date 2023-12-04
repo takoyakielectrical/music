b06db3ef6b0a281f8fc05941d6e8468bccf09e72
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-04 09:46:34 +0900

    これからよろしくお願いします。

---
+++ src/main/java/jp/ac/hcs/s3a120/main/mainController.java
@@ -0,0 +1,12 @@
+package jp.ac.hcs.s3a120.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
