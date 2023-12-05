718a1f12fb4b857796248f674b39afe438f3bf49
s20213015 <s20213015@HCS21052.stdlan.hcs.ac.jp>
2023-12-05 11:22:18 +0900

    コミット

--- src/main/resources/static/js/imgtrun.js
+++ src/main/resources/static/js/imgtrun.js
@@ -1,34 +1,16 @@
-// 画像のパスの配列
-const imagePaths = [
-  '../static/img/リオセスリ.png',
-  '../static/img/水龍.png'
-];
+var slideIndex = 0;
+showSlides();
 
-// 画像を表示する要素の取得
-const animationFrame = document.getElementById('animationFrame');
-
-// アニメーションの感覚（ミリ秒単位）
-const interval = 100; // 0.1秒
-
-// 現在のフレームのインデックス
-let currentFrameIndex = 0;
-
-// アニメーションを開始する関数
-function startAnimation() {
-  // 画像のパスを設定
-  animationFrame.src = imagePaths[currentFrameIndex];
-
-  // 次のフレームへ移動
-  currentFrameIndex++;
-
-  // 最後のフレームまで到達した場合、最初のフレームに戻る
-  if (currentFrameIndex === imagePaths.length) {
-    currentFrameIndex = 0;
-  }
-
-  // 次のフレームを表示するまで再帰的に呼び出す
-  setTimeout(startAnimation, interval);
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // 2秒ごとに画像を切り替える場合
 }
-
-// アニメーションを開始
-startAnimation();
