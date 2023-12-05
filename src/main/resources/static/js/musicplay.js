d6f2757b3a958e0c9c6e1d8e42c984fd430a152d
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-05 11:10:34 +0900

    音楽追加。途中から再生不可

--- src/main/resources/static/js/musicplay.js
+++ src/main/resources/static/js/musicplay.js
@@ -1,36 +1,52 @@
 
-/**
- * 
- */
 
-const urls = [];
 const musics = [];
-function play(url) {
-	urls.push(url);
-	for (const url of urls) {
-		const music = new Audio(url);
-		musics.push(music);
-	}
-	console.log(urls)
+let iiyo = true;
+let isPlaying = false;
 
+async function playMusic(index) {
+    return new Promise((resolve) => {
+        const music = musics[index];
 
-	function playMusic() {
-		return new Promise((resolve) => {
-			for (const music of musics) {
-				music.addEventListener('ended', resolve); // 再生が終わったらresolve
-				music.play();
-				
-			}
-		});
-	}
+        if (index === 0) {
+            music.addEventListener('ended', () => {
+                resolve();
+            });
+        }
 
-	async function playLoop() {
-		while (true) {
-			await playMusic();
-		}
-	}
-
-	playLoop();
+        iiyo = true;
+        music.play()
+            .then(() => {
+                iiyo = false;
+                console.log(iiyo);
+            })
+            .catch((error) => {
+                console.error("Error playing music:", error);
+                resolve(); // Resolve the promise even if there is an error
+            });
+    });
 }
 
+async function playLoop() {
+    while (true) {
+        if (!isPlaying) {
+            isPlaying = true;
+            await Promise.all(musics.map((music, index) => playMusic(index)));
+            isPlaying = false;
+        }
+    }
+}
+
+// ボタンのクリックごとに新しい音楽が追加され、新しい playLoop が開始されるのを防ぐ
+// 1つの playLoop で全ての音楽が再生されるように修正
+async function play(url) {
+    const music = new Audio(url);
+    musics.push(music);
+
+    console.log(url);
+
+    if (!isPlaying) {
+        playLoop();
+    }
+}
 
