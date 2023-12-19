f67bcb1d20efba878971605837b0f61d545ffff9
s20213102 <s20213102@HCS21060.stdlan.hcs.ac.jp>
2023-12-19 10:38:32 +0900

    ファイル

--- src/main/resources/static/js/imgtrun.js
+++ src/main/resources/static/js/imgtrun.js
@@ -1,27 +1,30 @@
 var slideIndex = 0;
-var slideshowPaused = false;
-showSlides();
+var slideshowPaused = true;
+showSlides()
+
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-     slideInterval = setTimeout(showSlides, 70); //0.1秒ごとに画像を切り替える場合
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+	slides[slideIndex - 1].style.display = "block";
+	if (!slideshowPaused) {
+		slideInterval = setTimeout(showSlides, 70); //0.1秒ごとに画像を切り替える場合
+	}
 }
 function toggleSlideshow() {
-    
-    if (slideshowPaused) {
-        slideshowPaused = false;
-        showSlides();
-    } else {
-        slideshowPaused = true;
-        clearTimeout(slideInterval);
-    }
+
+	if (slideshowPaused) {
+		slideshowPaused = false;
+		showSlides();
+	} else {
+		slideshowPaused = true;
+		clearTimeout(slideInterval);
+	}
 }
