1e87889b9f611c8cadca4833f52d9750d04e7e16
s20213060 <s20213060@HCS21059.stdlan.hcs.ac.jp>
2023-12-20 10:35:57 +0900

    Thank you for maked common commit

---
+++ src/main/resources/static/js/changeString.js
@@ -0,0 +1,36 @@
+
+document.getElementById('red1').addEventListener('click', () => {
+	var inputElement = document.getElementById('red1');
+	if (inputElement.value === 'PUSH!') {
+		inputElement.value = 'PUSHED!';
+		setTimeout(function() {
+			inputElement.value = 'PUSH!';
+		}, 3000);
+	}
+});
+document.getElementById('red2').addEventListener('click', () => {
+});
+document.getElementById('red3').addEventListener('click', () => {
+});
+document.getElementById('red4').addEventListener('click', () => {
+});
+document.getElementById('blue1').addEventListener('click', () => {
+});
+document.getElementById('blue2').addEventListener('click', () => {
+});
+document.getElementById('blue3').addEventListener('click', () => {
+});
+document.getElementById('blue4').addEventListener('click', () => {
+});
+document.getElementById('blue5').addEventListener('click', () => {
+});
+document.getElementById('green1').addEventListener('click', () => {
+});
+document.getElementById('green2').addEventListener('click', () => {
+});
+document.getElementById('green3').addEventListener('click', () => {
+});
+document.getElementById('green4').addEventListener('click', () => {
+});
+document.getElementById('green5').addEventListener('click', () => {
+});
