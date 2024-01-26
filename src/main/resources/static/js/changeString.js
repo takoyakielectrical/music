8b044266d042801111a024d04c8ee91f1971a4ae
s20213015 <s20213015@HCS21052.stdlan.hcs.ac.jp>
2024-01-26 10:01:26 +0900

    音追加、昨日までのやつ

--- src/main/resources/static/js/changeString.js
+++ src/main/resources/static/js/changeString.js
@@ -1,116 +1,232 @@
+/** black ticket */
+
+/** blue ticket */
+
+/** red ticket */
 document.getElementById('red1').addEventListener('click', () => {
 	var inputElement = document.getElementById('1');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
-		inputElement.class = 'brightbtn';
-		console.log(inputElement.class);
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
+		
 	}else{
 		inputElement.src = '/img/light/dark.png';
-		inputElement.class = 'darkbtn';
-		console.log(inputElement.class);
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('red2').addEventListener('click', () => {
 	var inputElement = document.getElementById('2');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('red3').addEventListener('click', () => {
 	var inputElement = document.getElementById('3');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('red4').addEventListener('click', () => {
 	var inputElement = document.getElementById('4');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('blue1').addEventListener('click', () => {
 	var inputElement = document.getElementById('5');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('blue2').addEventListener('click', () => {
 	var inputElement = document.getElementById('6');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('blue3').addEventListener('click', () => {
 	var inputElement = document.getElementById('7');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('blue4').addEventListener('click', () => {
 	var inputElement = document.getElementById('8');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('blue5').addEventListener('click', () => {
 	var inputElement = document.getElementById('9');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('green1').addEventListener('click', () => {
 	var inputElement = document.getElementById('10');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('green2').addEventListener('click', () => {
 	var inputElement = document.getElementById('11');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('green3').addEventListener('click', () => {
 	var inputElement = document.getElementById('12');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('green4').addEventListener('click', () => {
 	var inputElement = document.getElementById('13');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
 document.getElementById('green5').addEventListener('click', () => {
 	var inputElement = document.getElementById('14');
 	if (inputElement.src === 'http://localhost:8080/img/light/dark.png') {
 		inputElement.src = '/img/light/bright.png';
+		inputElement.style.height = '80px';
+		inputElement.style.width = '80px';
+		inputElement.style.marginTop = '-12.8px';
+		inputElement.style.marginLeft = '-7px';
 	}else{
 		inputElement.src = '/img/light/dark.png';
+		inputElement.style.height = '60px';
+		inputElement.style.width = '60px';
+		inputElement.style.marginTop = '0';
+		inputElement.style.marginLeft = '0';
 	}
 });
+
+/** new white ticket */
